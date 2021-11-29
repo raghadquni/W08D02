0faaae618f504a27ce29e23dbf7efdddabfbda48
raghadquni <Raghad.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-29 21:33:21 +0300

    task1

--- routers/controller/user.js
+++ routers/controller/user.js
@@ -1,13 +1,15 @@
 const userModel = require("../../db/models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { options } = require("../routes/role");
 
 
-const salt = Number(process.env.SALT); 
+const secret = process.env.SECRET_KEY;
 
 const register = async (req, res) => {
   const { email, password, role } = req.body;
 
+  const salt = Number(process.env.SALT);
   const savedEmail = email.toLowerCase();
   const savedPassword = await bcrypt.hash(password, salt);
 
@@ -19,10 +21,10 @@ const newUser = new userModel({
   newUser
     .save()
     .then((result) => {
-      res.json(result);
+      res.status(201).json(result);
     })
     .catch((err) => {
-      res.send(err);
+      res.status(400).send(err);
     });
 };
 
@@ -40,29 +42,37 @@ const getUsers = (req, res) => {
 
 const login = (req, res) => {
     const { email, password } = req.body;
-    
-    userModel
-    .findOne({ email })
-    .then((result) => {
-        if (result) {
-            if (result.email == email) {
-                if (result.password == password) {
-                    res.status(200).json(result);
-                } else {
-                    res.status(400).json("invalid email or password");
-                }
-            } else {
-                res.status(400).json("invalid email or password");
-            }
-        } else {
-            res.status(404).json(" email does not exsist");
-        }
-})
-.catch((err) => {
-    res.status(400).res.json(err);
-});
-}
+      const savedEmail = email.toLowerCase();
+  userModel
+    .findOne({ email: savedEmail })
+    .then(async (result) => {
+      if (result) {
+        if (result.email == email) {
+          const hashedPass = await bcrypt.compare(password, result.password);
+          const payload = {
+            email,
+          };
 
+          if (hashedPass) {
+            const options = {
+                expiresIn : "60m"
+            }; 
+            let token = jwt.sign(payload, secret, options);
+            res.status(200).json({ result, token });
+          } else {
+            res.status(400).json("Wrong email or password");
+          }
+        } else {
+          res.status(400).json("Wrong email or password");
+        }
+      } else {
+        res.status(404).json("Email not exist");
+      }
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
 
 
 
