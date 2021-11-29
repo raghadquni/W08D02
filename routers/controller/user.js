c512e68282c1ba1c3d908f789dabe1a1ee00c924
raghadquni <Raghad.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-29 19:57:27 +0300

    task1

--- routers/controller/user.js
+++ routers/controller/user.js
@@ -18,4 +18,43 @@ const register = (req, res) => {
     });
 };
 
-module.exports = {register}
+const getUsers = (req, res) => {
+    userModel
+      .find({})
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  };
+
+
+const login = (req, res) => {
+    const { email, password } = req.body;
+    userModel
+    .findOne({ email })
+    .then((result) => {
+        if (result) {
+            if (result.email == email) {
+                if (result.password == password) {
+                    res.status(200).json(result);
+                } else {
+                    res.status(400).json("invalid email or password");
+                }
+            } else {
+                res.status(400).json("invalid email or password");
+            }
+        } else {
+            res.status(404).json(" email does not exsist");
+        }
+})
+.catch((err) => {
+    res.status(400).res.json(err);
+});
+}
+
+
+
+
+module.exports = {register, getUsers, login}
--- routers/routes/user.js
+++ routers/routes/user.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const userRouter  = express.Router();
 
-const {register} = require("../controller/user")
+const {register, getUsers, login} = require("../controller/user")
 
 userRouter.post("/signup", register)
+userRouter.get("/getUsers", getUsers)
+userRouter.post("/login", login);
+
+
 
 module.exports = userRouter;
