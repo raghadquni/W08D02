ab1b33177650a822943074863330428ef845e1af
raghadquni <Raghad.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-29 18:25:18 +0300

    task1

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 require("dotenv").config(); // اقدر اكتبها كذا باختصار
+const db = require("./db/index.js");
+
 
 // instantiate express
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
